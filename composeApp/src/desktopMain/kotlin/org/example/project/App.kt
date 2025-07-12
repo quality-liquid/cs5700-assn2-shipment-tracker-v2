904f6665aea583349c1ef5b1eddb074499e5fcfe
Benjamin <hickenlooperbe@gmail.com>
2025-07-12 14:22:02 -0600

    played around with jetpack compose to get a feel for it

--- composeApp/src/desktopMain/kotlin/org/example/project/App.kt
+++ composeApp/src/desktopMain/kotlin/org/example/project/App.kt
@@ -3,12 +3,14 @@ package org.example.project
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -21,22 +23,29 @@ import assn2_shipment_tracker_v2.composeapp.generated.resources.compose_multipla
 @Composable
 @Preview
 fun App() {
+    var text by remember { mutableStateOf("") }
+    val output_text = remember { mutableStateListOf<String>() }
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose:")
+        Column {
+            Row {
+                TextField(
+                    value = text,
+                    onValueChange = { newText ->
+                        text = newText  // Update the state
+                    },
+                    label = { Text("Shipment ID: ") }
+                )
+                Button(
+                    {
+                        output_text.add(text)
+                        text = ""
+                    }
+                ) {
+                    Text("Print text")
                 }
+            }
+            output_text.forEach { text: String ->
+                Text(text)
             }
         }
     }
